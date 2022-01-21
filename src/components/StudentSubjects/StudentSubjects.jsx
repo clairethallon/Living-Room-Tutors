5eed975e67080f934be55408ec2d19adae21f622
Laura Burbank <laura.burbank@gmail.com>
2022-01-20 18:35:28 -0600

    subjects added to student page

--- src/components/StudentSubjects/StudentSubjects.jsx
+++ src/components/StudentSubjects/StudentSubjects.jsx
@@ -10,9 +10,173 @@ function StudentSubjects(props) {
   const store = useSelector((store) => store);
   const [heading, setHeading] = useState('Student Subjects');
 
+  const[newPrimarySubject, setNewPrimarySubject]= useState();
+  const[newSecondarySubject, setNewSecondarySubject]= useState();
+  const[newTertiarySubject, setNewTertiarySubject]= useState();
+
+  const changePrimarySubject= ()=>{
+    console.log('in new primary');
+    setNewPrimarySubject(event.target.value);
+  }
+
+  const changeSecondarySubject= ()=>{
+    console.log('in new secondary');
+    setNewSecondarySubject(event.target.value);
+  }
+
+  const changeTertiarySubject= ()=>{
+    console.log('in new tertiary');
+    setNewTertiarySubject(event.target.value);
+  }
+
   return (
     <div>
       <h2>{heading}</h2>
+
+
+      <label for="studentSubjects">In what subjects does the student need the most support? (1st Choice)</label>
+
+        <select name="gradeLevel" onChange={(event)=>changePrimarySubject(event)} title="Student's Current Grade Level">
+          <option value="K-5 Math">K-5 Math</option>
+          <option value="K-5 Reading">K-5 Reading</option>
+          <option value="K-5 English/Writing">K-5 English/Writing</option>
+          <option value="K-5 Social Studies">K-5 Social Studies</option>
+          <option value="K-5 Science">K-5 Science</option>
+          <option value="6-8th Language Arts">6-8th Language Arts</option>
+          <option value="6-8th Science">6-8th Science</option>
+          <option value="6-8th Social Studies">6-8th Social Studies</option>
+          <option value="Math: Pre-Algebra">Math: Pre-Algebra</option>
+          <option value="Math: Algebra 1/Linear Algebra">Math: Algebra I/Linear Algebra</option>
+          <option value="Math: Algebra II">Math: Algebra II</option>
+          <option value="Math: Geometry">Math: Geometry</option>
+          <option value="Math: Precalculus/Trigonometry">Math: Precalculus/Trigonometry</option>
+          <option value="Science: Biology/Life Sciences">Science: Biology/Life Sciences</option>
+          <option value="Science: Chemistry">Science: Chemistry</option>
+          <option value="Science: Physics">Science: Physics</option>
+          <option value="Science: Computer Science">Science: Computer Science</option>
+          <option value="Language: Chinese">Language: Chinese </option>
+          <option value="Language: Spanish">Language: Spanish</option>
+          <option value="Language: French">Language: French</option>
+          <option value="Language: German">Language: German</option>
+          <option value="History: World History">History: World History</option>
+          <option value="History: U.S. History">History: U.S. History</option>
+          <option value="AP Honors Biology">AP Honors Biology</option>
+          <option value="AP Honors Chemistry">AP Honors Chemistry</option>
+          <option value="AP Honors Physics">AP Honors Physics</option>
+          <option value="AP Honors Calculus AB">AP Honors Calculus AB</option>
+          <option value="AP Honors Calculus BC">AP Honors Calculus BC</option>
+          <option value="AP Honors Statistics">AP Honors Statistics</option>
+          <option value="AP Honors Computer Science">AP Honors Computer Science</option>
+          <option value="AP Honors English Literature and Composition">AP Honors English Literature and Composition</option>
+          <option value="AP Honors English Language and Composition">AP Honors English Language and Composition</option>
+          <option value="AP Honors Macroeconomics">AP Honors Macroeconomics</option>
+          <option value="AP Honors Microeconomics">AP Honors Microeconomics</option>
+          <option value="AP Honors Psychology">AP Honors Psychology</option>
+          <option value="AP Honors United States History">AP Honors United States History</option>
+          <option value="AP Honors Government and Politics">AP Honors Government and Politics</option>
+          <option value="AP Honors Human Geography">AP Honors Human Geography</option>
+          <option value="SAT Subject Tests">SAT Subject Tests</option>
+          <option value="SAT Prep">SAT Prep</option>
+          <option value="ACT Prep">ACT Prep</option>
+          <option value="Other">Other</option>
+        </select>
+
+
+        <label for="studentSubjects">In what subjects does the student need the most support? (2nd Choice)</label>
+
+        <select name="gradeLevel" onChange={(event)=>changeSecondarySubject(event)} title="Student's Current Grade Level">
+          <option value="K-5 Math">K-5 Math</option>
+          <option value="K-5 Reading">K-5 Reading</option>
+          <option value="K-5 English/Writing">K-5 English/Writing</option>
+          <option value="K-5 Social Studies">K-5 Social Studies</option>
+          <option value="K-5 Science">K-5 Science</option>
+          <option value="6-8th Language Arts">6-8th Language Arts</option>
+          <option value="6-8th Science">6-8th Science</option>
+          <option value="6-8th Social Studies">6-8th Social Studies</option>
+          <option value="Math: Pre-Algebra">Math: Pre-Algebra</option>
+          <option value="Math: Algebra 1/Linear Algebra">Math: Algebra I/Linear Algebra</option>
+          <option value="Math: Algebra II">Math: Algebra II</option>
+          <option value="Math: Geometry">Math: Geometry</option>
+          <option value="Math: Precalculus/Trigonometry">Math: Precalculus/Trigonometry</option>
+          <option value="Science: Biology/Life Sciences">Science: Biology/Life Sciences</option>
+          <option value="Science: Chemistry">Science: Chemistry</option>
+          <option value="Science: Physics">Science: Physics</option>
+          <option value="Science: Computer Science">Science: Computer Science</option>
+          <option value="Language: Chinese">Language: Chinese </option>
+          <option value="Language: Spanish">Language: Spanish</option>
+          <option value="Language: French">Language: French</option>
+          <option value="Language: German">Language: German</option>
+          <option value="History: World History">History: World History</option>
+          <option value="History: U.S. History">History: U.S. History</option>
+          <option value="AP Honors Biology">AP Honors Biology</option>
+          <option value="AP Honors Chemistry">AP Honors Chemistry</option>
+          <option value="AP Honors Physics">AP Honors Physics</option>
+          <option value="AP Honors Calculus AB">AP Honors Calculus AB</option>
+          <option value="AP Honors Calculus BC">AP Honors Calculus BC</option>
+          <option value="AP Honors Statistics">AP Honors Statistics</option>
+          <option value="AP Honors Computer Science">AP Honors Computer Science</option>
+          <option value="AP Honors English Literature and Composition">AP Honors English Literature and Composition</option>
+          <option value="AP Honors English Language and Composition">AP Honors English Language and Composition</option>
+          <option value="AP Honors Macroeconomics">AP Honors Macroeconomics</option>
+          <option value="AP Honors Microeconomics">AP Honors Microeconomics</option>
+          <option value="AP Honors Psychology">AP Honors Psychology</option>
+          <option value="AP Honors United States History">AP Honors United States History</option>
+          <option value="AP Honors Government and Politics">AP Honors Government and Politics</option>
+          <option value="AP Honors Human Geography">AP Honors Human Geography</option>
+          <option value="SAT Subject Tests">SAT Subject Tests</option>
+          <option value="SAT Prep">SAT Prep</option>
+          <option value="ACT Prep">ACT Prep</option>
+          <option value="Other">Other</option>
+        </select>
+
+
+        <label for="studentSubjects">In what subjects does the student need the most support? (3rd Choice)</label>
+
+        <select name="gradeLevel" onChange={(event)=>changeTertiarySubject(event)} title="Student's Current Grade Level">
+          <option value="K-5 Math">K-5 Math</option>
+          <option value="K-5 Reading">K-5 Reading</option>
+          <option value="K-5 English/Writing">K-5 English/Writing</option>
+          <option value="K-5 Social Studies">K-5 Social Studies</option>
+          <option value="K-5 Science">K-5 Science</option>
+          <option value="6-8th Language Arts">6-8th Language Arts</option>
+          <option value="6-8th Science">6-8th Science</option>
+          <option value="6-8th Social Studies">6-8th Social Studies</option>
+          <option value="Math: Pre-Algebra">Math: Pre-Algebra</option>
+          <option value="Math: Algebra 1/Linear Algebra">Math: Algebra I/Linear Algebra</option>
+          <option value="Math: Algebra II">Math: Algebra II</option>
+          <option value="Math: Geometry">Math: Geometry</option>
+          <option value="Math: Precalculus/Trigonometry">Math: Precalculus/Trigonometry</option>
+          <option value="Science: Biology/Life Sciences">Science: Biology/Life Sciences</option>
+          <option value="Science: Chemistry">Science: Chemistry</option>
+          <option value="Science: Physics">Science: Physics</option>
+          <option value="Science: Computer Science">Science: Computer Science</option>
+          <option value="Language: Chinese">Language: Chinese </option>
+          <option value="Language: Spanish">Language: Spanish</option>
+          <option value="Language: French">Language: French</option>
+          <option value="Language: German">Language: German</option>
+          <option value="History: World History">History: World History</option>
+          <option value="History: U.S. History">History: U.S. History</option>
+          <option value="AP Honors Biology">AP Honors Biology</option>
+          <option value="AP Honors Chemistry">AP Honors Chemistry</option>
+          <option value="AP Honors Physics">AP Honors Physics</option>
+          <option value="AP Honors Calculus AB">AP Honors Calculus AB</option>
+          <option value="AP Honors Calculus BC">AP Honors Calculus BC</option>
+          <option value="AP Honors Statistics">AP Honors Statistics</option>
+          <option value="AP Honors Computer Science">AP Honors Computer Science</option>
+          <option value="AP Honors English Literature and Composition">AP Honors English Literature and Composition</option>
+          <option value="AP Honors English Language and Composition">AP Honors English Language and Composition</option>
+          <option value="AP Honors Macroeconomics">AP Honors Macroeconomics</option>
+          <option value="AP Honors Microeconomics">AP Honors Microeconomics</option>
+          <option value="AP Honors Psychology">AP Honors Psychology</option>
+          <option value="AP Honors United States History">AP Honors United States History</option>
+          <option value="AP Honors Government and Politics">AP Honors Government and Politics</option>
+          <option value="AP Honors Human Geography">AP Honors Human Geography</option>
+          <option value="SAT Subject Tests">SAT Subject Tests</option>
+          <option value="SAT Prep">SAT Prep</option>
+          <option value="ACT Prep">ACT Prep</option>
+          <option value="Other">Other</option>
+        </select>
+
     </div>
   );
 }
