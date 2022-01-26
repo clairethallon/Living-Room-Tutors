f27f680c175573021d339b50a341ac988bbd9ce0
Hannah Fligel <fligelhannah@gmail.com>
2022-01-26 12:31:55 -0600

    useHistory added to match button to navigate to match page on click of match

--- src/components/MatchButton/MatchButton.js
+++ src/components/MatchButton/MatchButton.js
@@ -1,19 +1,24 @@
 import React, { useState } from "react";
 import { useSelector } from "react-redux";
 import { Button } from "react-bootstrap";
+import { useHistory } from "react-router-dom";
 
 // Basic functional component structure for React with default state
 // value setup. When making a new component be sure to replace the
 // component name MatchButton with the name for the new component.
 function MatchButton(props) {
-  // Using hooks we're creating local state for a "heading" variable with
-  // a default value of 'Functional Component'
+  const history = useHistory();
+
   const store = useSelector((store) => store);
   const [heading, setHeading] = useState("Functional Component");
 
+  const navigateToMatchPage = () => {
+    history.push("/match");
+  };
+
   return (
     <div>
-      <Button>Match</Button>
+      <Button onClick={navigateToMatchPage}>Match</Button>
     </div>
   );
 }
