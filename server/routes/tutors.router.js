8f6baf9e7237712d536f1a916fc605d30bef1876
Claire Thallon <84248652+clairethallon@users.noreply.github.com>
2022-01-26 21:49:47 -0600

    Revert "Route updates"

--- server/routes/tutors.router.js
+++ server/routes/tutors.router.js
@@ -21,34 +21,9 @@ router.get("/", (req, res) => {
 });
 
 router.get("/active", (req, res) => {
-  const query = `  SELECT *, 
-  "1st",
- "2nd",
- "3rd",
- "4th",
- "5th",
- "6th",
- "7th",
- "8th",
- "9th",
- "10th",
- "11th",
- "12th",
- language."Arabic",
- language."Chinese",
- language."French",
- language."Hmong",
- language."Somali",
- language."Tagalog",
- language."Vietnamese",
- language."Spanish",
- language."Other"
-  FROM tutors
-   JOIN subjects_tutors ON tutors.subjects_id = subjects_tutors.id
-   JOIN "language" ON tutors.language_tutor_id = "language".id
-   JOIN mentoring_grade ON tutors.mentoring_grade_id = mentoring_grade.id
-   WHERE tutors.active_tutor = true and tutors.matched = false 
-   ORDER BY submission_timestamp ASC;`;
+  const query = `SELECT * FROM tutors
+  WHERE tutors.active_tutor = true
+  ORDER BY submission_timestamp ASC;`;
   pool
     .query(query)
     .then((result) => {
@@ -61,34 +36,9 @@ router.get("/active", (req, res) => {
 });
 
 router.get("/deactive", (req, res) => {
-  const query = `  SELECT *, 
-  "1st",
- "2nd",
- "3rd",
- "4th",
- "5th",
- "6th",
- "7th",
- "8th",
- "9th",
- "10th",
- "11th",
- "12th",
- language."Arabic",
- language."Chinese",
- language."French",
- language."Hmong",
- language."Somali",
- language."Tagalog",
- language."Vietnamese",
- language."Spanish",
- language."Other"
-  FROM tutors
-   JOIN subjects_tutors ON tutors.subjects_id = subjects_tutors.id
-   JOIN "language" ON tutors.language_tutor_id = "language".id
-   JOIN mentoring_grade ON tutors.mentoring_grade_id = mentoring_grade.id
-   WHERE tutors.active_tutor = false and tutors.matched = false 
-   ORDER BY submission_timestamp ASC;`;
+  const query = `SELECT * FROM tutors
+  WHERE tutors.active_tutor = false
+  ORDER BY submission_timestamp ASC;`;
   pool
     .query(query)
     .then((result) => {
