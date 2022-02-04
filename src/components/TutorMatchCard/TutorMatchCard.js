a3c1dcde95c317809e59f6a132139a37a5cdbd66
Claire Thallon <clairethallon@gmail.com>
2022-02-04 13:34:04 -0600

    subject displaying in match accordian header columns

--- src/components/TutorMatchCard/TutorMatchCard.js
+++ src/components/TutorMatchCard/TutorMatchCard.js
@@ -18,12 +18,78 @@ function TutorMatchCard(props) {
   const languageFilter = useSelector((store) => store.languageFilter);
   const selected_tutee = useSelector((store) => store.selected_tutee);
 
-  const languageCheck = () => {
-    console.log('in lang check', languageFilter, props.tutor);
-    if (languageFilter === 'Spanish') {
-      console.log('SPANISHHH')
+  const [subject1, setSubject1] = useState("");
+  const [subject2, setSubject2] = useState("");
+  const [subject3, setSubject3] = useState("");
+
+  useEffect(() => {
+    subjectFinder(subjects);
+  }, []);
+
+  const subjects = [
+    { name: "K-5 Math ", dbname: "K5_Math" },
+    { name: "K-5 Reading ", dbname: "K5_Reading" },
+    { name: "K-5 English/Writing ", dbname: "K5_English_Writing" },
+    { name: "K-5 Social Studies ", dbname: "K5_Science" },
+    { name: "K-5 Science ", dbname: "K5_social_studies" },
+    { name: "6-8th Language Arts ", dbname: "6th_to_8th_language_arts" },
+    { name: "6-8th Science ", dbname: "6th_to_8th_science" },
+    { name: "6-8th Social Studies ", dbname: "6th_to_8th_social_studies" },
+    { name: "Pre-Algebra ", dbname: "math_pre_algebra" },
+    { name: "Algebra I/ Linear Algebra ", dbname: "math_alg1_linear_alg" },
+    { name: "Algebra II ", dbname: "math_alg2" },
+    { name: "Geometry ", dbname: "math_geom" },
+    { name: "Precalculus/Trigonometry ", dbname: "math_precalc_trig" },
+    { name: "Biology/Life Sciences ", dbname: "sci_bio_life" },
+    { name: "Chemistry ", dbname: "sci_chem" },
+    { name: "Physics ", dbname: "sci_physics" },
+    { name: "Computer Science ", dbname: "sci_comp_sci" },
+    { name: "Chinese ", dbname: "lang_chinese" },
+    { name: "Spanish ", dbname: "lang_spanish" },
+    { name: "French ", dbname: "lang_french" },
+    { name: "German ", dbname: "lang_german" },
+    { name: "World History ", dbname: "hist_world" },
+    { name: "U.S. History ", dbname: "hist_us" },
+    { name: "AP/Honors Biology ", dbname: "ap_bio" },
+    { name: "AP/Honors Chemistry ", dbname: "ap_chem" },
+    { name: "AP/Honors Physics ", dbname: "ap_physics" },
+    { name: "AP/Honors Calculus AB ", dbname: "ap_calc_AB" },
+    { name: "AP/Honors Calculus BC ", dbname: "ap_calc_BC" },
+    { name: "AP/Honors Statistics ", dbname: "ap_stats" },
+    { name: "AP/Honors Computer Science ", dbname: "ap_comp_sci" },
+    {
+      name: "AP/Honors English Literature and Composition ",
+      dbname: "ap_english_lit_comp",
+    },
+    { name: "AP/Honors Language and Composition ", dbname: "ap_lang_comp" },
+    { name: "AP/Honors Macroeconomics ", dbname: "ap_macro_econ" },
+    { name: "AP/Honors Microeconomics ", dbname: "ap_micro_econ" },
+    { name: "AP/Honors Psychology ", dbname: "ap_psyc" },
+    { name: "AP/Honors United States History ", dbname: "ap_hist_us" },
+    {
+      name: "AP/Honors Government and Politics (US) ",
+      dbname: "ap_gov_politics_us",
+    },
+    { name: "AP/Honors Human Geography ", dbname: "ap_human_geog" },
+    { name: "SAT Subject Tests ", dbname: "sat_subject_tests" },
+    { name: "SAT Prep ", dbname: "sat_prep" },
+    { name: "ACT Prep ", dbname: "act_prep" },
+  ];
+
+  const subjectFinder = (subjects) => {
+    console.log("in subjectFinder");
+    for (let i = 0; i < subjects.length; i++) {
+      if (subjects[i].dbname == selected_tutee.subject_1) {
+        setSubject1(subjects[i].name);
+      }
+      if (subjects[i].dbname == selected_tutee.subject_2) {
+        setSubject2(subjects[i].name);
+      }
+      if (subjects[i].dbname == selected_tutee.subject_3) {
+        setSubject3(subjects[i].name);
+      }
     }
-  }
+  };
 
   return (
     <div>
@@ -46,7 +112,7 @@ function TutorMatchCard(props) {
                 {props.tutor.tutor_first_name} {props.tutor.tutor_last_name}
               </Col>
               <Col xs="2">{props.tutor.submission_timestamp}</Col>
-              <Col xs="3">{selected_tutee.subject_1}</Col>
+              <Col xs="3">{subject1}</Col>
               <Col xs="1" className="flaggedSubjectLanguage">
                 <SubjectFlag />
                 <LanguageFlag />
