fc220914f711ee217ee111f781032e4e9e659d7c
Hannah Fligel <fligelhannah@gmail.com>
2022-02-08 16:30:14 -0800

    icons and active background color added

--- src/components/AdminNavBar/AdminNavBar.js
+++ src/components/AdminNavBar/AdminNavBar.js
@@ -1,9 +1,15 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, NavLink } from "react-router-dom";
 import LogOutButton from "../LogOutButton/LogOutButton";
 import { useSelector } from "react-redux";
 import "../AdminNavBar/AdminNavBar.css";
 import LRTHeader from "../../images/LRTHeader.png";
+import tuteeIconBlue from "../../images/tuteeIconBlue.svg";
+import tuteeIconWhite from "../../images/tuteeIconWhite.svg";
+import tutorIconBlue from "../../images/tutorIconBlue.svg";
+import tutorIconWhite from "../../images/tutorIconWhite.svg";
+import recordsIconBlue from "../../images/recordsIconBlue.svg";
+import recordsIconWhite from "../../images/recordsIconWhite.svg";
 
 function AdminNavBar() {
   const user = useSelector((store) => store.user);
@@ -26,17 +32,32 @@ function AdminNavBar() {
       {user.id && (
         <div className="adminNavContents">
           <div className="AdminNavLinks">
-            <Link className="AdminNavLink" to="/admin">
+            <NavLink
+              activeClassName="tuteeNavbarLink--active navbarlink--active"
+              className="AdminNavLink"
+              to="/admin"
+            >
+              <div className="tuteeIcon"></div>
               Tutees
-            </Link>
+            </NavLink>
 
-            <Link className="AdminNavLink" to="/tutors">
+            <NavLink
+              activeClassName="tutorNavbarLink--active navbarlink--active"
+              className="AdminNavLink"
+              to="/tutors"
+            >
+              <div className="tutorIcon"></div>
               Tutors
-            </Link>
+            </NavLink>
 
-            <Link className="AdminNavLink" to="/records">
+            <NavLink
+              activeClassName="recordsNavbarLink--active navbarlink--active"
+              className="AdminNavLink"
+              to="/records"
+            >
+              <div className="recordsIcon"></div>
               Records
-            </Link>
+            </NavLink>
           </div>
           <div className="logoutButton">
             <LogOutButton />
