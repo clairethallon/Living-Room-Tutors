e7ac33302cdbb230f9b9dd7788f03cc0a60dfa35
Hannah Fligel <fligelhannah@gmail.com>
2022-02-11 17:49:17 -0600

    tutee form styling complete

--- src/components/StudentModal/StudentModal.jsx
+++ src/components/StudentModal/StudentModal.jsx
@@ -3,12 +3,17 @@ import { useDispatch, useSelector } from "react-redux";
 import { Link } from "react-router-dom";
 import Button from "react-bootstrap/Button";
 import Modal from "react-bootstrap/Modal";
+import { useHistory } from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCheckCircle } from "@fortawesome/free-solid-svg-icons";
 
 function StudentModal(props) {
   const dispatch = useDispatch();
   const store = useSelector((store) => store);
+  const history = useHistory();
 
   const [show, setShow] = useState(false);
+  const checkIcon = <FontAwesomeIcon icon={faCheckCircle} />;
 
   const handleClose = () => {
     setShow(false);
@@ -31,22 +36,38 @@ function StudentModal(props) {
           {" "}
           Submit{" "}
         </Button>
-        <Modal show={show} onHide={handleClose}>
-          <Modal.Header closeButton>
-            <Modal.Title>Thank you for your application!</Modal.Title>
-          </Modal.Header>
+        <Modal className="submissionModal" show={show} onHide={handleClose}>
+          <Modal.Header
+            className="submissionModalHeader"
+            closeButton
+          ></Modal.Header>
 
           <Modal.Body>
-            <p>
-              Your submitted application will be reviewed by one of our members
-              you will be contacted via email regarding your eligibility.
-            </p>
+            <div className="checkIcon">{checkIcon}</div>
+            <div className="submissionModalTextContainer">
+              <center>
+                <h1 className="submissionModalH1">
+                  Thank you for your submission!
+                </h1>
+              </center>
+              <center>
+                <p>
+                  You application was submitted successfully and will be
+                  reviewed by one of our team members. Once a match is found,
+                  you will be notified via email.
+                </p>
+              </center>
+            </div>
           </Modal.Body>
 
-          <Modal.Footer>
-            <Link to="/StudentInfo">
-              <Button variant="primary" onClick={handleClose}>
-                Home
+          <Modal.Footer className="submissionModalFooter">
+            <Link to="/Home">
+              <Button
+                className="primaryButton okButton"
+                variant="primary"
+                onClick={handleClose}
+              >
+                OK
               </Button>
             </Link>
           </Modal.Footer>
