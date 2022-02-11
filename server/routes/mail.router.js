de5a784b08707df715d4b507f602ca8a4a889f15
Miriam McNamara <miriammcnamara@icloud.com>
2022-02-11 15:08:30 -0600

    email post route working with authentication token

--- server/routes/mail.router.js
+++ server/routes/mail.router.js
@@ -29,73 +29,79 @@ oauth2Client.setCredentials({
 
 const accessToken = oauth2Client.getAccessToken();
 
-let transporter = nodemailer.createTransport({
-  host: "smtp.gmail.com",
-  port: 465,
-  secure: true,
-  auth: {
-    type: "OAuth2",
-    user: gconfig.mailUser,
-    clientId: gconfig.clientId,
-    clientSecret: gconfig.clientSecret,
-    refreshToken: gconfig.refreshToken,
-    accessToken: accessToken,
-  },
+router.post("/", cors(), async (req, res) => {
+  console.log("email post route hit");
+  console.log(req.body.email);
+  let email = req.body.email;
+
+  let transporter = nodemailer.createTransport({
+    host: "smtp.gmail.com",
+    port: 465,
+    secure: true,
+    auth: {
+      type: "OAuth2",
+      user: gconfig.mailUser,
+      clientId: gconfig.clientId,
+      clientSecret: gconfig.clientSecret,
+      refreshToken: gconfig.refreshToken,
+      accessToken: accessToken,
+    },
+  });
+
+  // transporter.verify((err, success) => {
+  //   err
+  //     ? console.log(err)
+  //     : console.log(`=== Server is ready to take messages: ${success} ===`);
+  // });
+
+  // let mailOptions = {
+  //   from: "test@gmail.com",
+  //   to: gconfig.mailUser,
+  //   subject: "Nodemailer API",
+  //   text: "Hi from your nodemailer API",
+  // };
+
+  // transporter.sendMail(mailOptions, function (err, data) {
+  //   if (err) {
+  //     console.log("Error " + err);
+  //   } else {
+  //     console.log("Email sent successfully");
+  //   }
+  // });
+  // });
+
+  /**
+   * POST route template
+   */
+  // router.post("/", cors(), async (req, res) => {
+  //   console.log("email post route hit");
+  //   console.log(req.body.email);
+  //   let email = req.body.email;
+
+  //   let transporter = nodemailer.createTransport({
+  //     host: "smtp.gmail.com",
+  //     port: 587,
+  //     secure: false, // true for 465, false for other ports
+  //     auth: {
+  //       user: "blahblah@gmail.com",
+  //       pass: "blooper",
+  //     },
+  //   });
+
+  let info = await transporter
+    .sendMail({
+      from: gconfig.mailUser, //not showing up in email
+      to: `${email}`,
+      subject: "test email",
+      text: "Thank you so much for contacting Living Room Tutors! This is a test. Thanks again, Living Room Tutors.",
+      html: `<div className="email" >
+      <h2>Thank you so much for contacting Living Room Tutors!</h2>
+      <p>This is a test.</p>
+      <p>Thanks again, Living Room Tutors</p>
+    </div>`,
+    })
+    .catch(console.error);
+  // console.log("Message sent: %s", info.messageId);
 });
-
-transporter.verify((err, success) => {
-  err
-    ? console.log(err)
-    : console.log(`=== Server is ready to take messages: ${success} ===`);
-});
-
-let mailOptions = {
-  from: "test@gmail.com",
-  to: gconfig.mailUser,
-  subject: "Nodemailer API",
-  text: "Hi from your nodemailer API",
-};
-
-transporter.sendMail(mailOptions, function (err, data) {
-  if (err) {
-    console.log("Error " + err);
-  } else {
-    console.log("Email sent successfully");
-  }
-});
-
-/**
- * POST route template
- */
-// router.post("/", cors(), async (req, res) => {
-//   console.log("email post route hit");
-//   console.log(req.body.email);
-//   let email = req.body.email;
-
-//   let transporter = nodemailer.createTransport({
-//     host: "smtp.gmail.com",
-//     port: 587,
-//     secure: false, // true for 465, false for other ports
-//     auth: {
-//       user: "blahblah@gmail.com",
-//       pass: "blooper",
-//     },
-//   });
-
-//   let info = await transporter
-//     .sendMail({
-//       from: "miriammcnamara@icloud.com", //not showing up in email
-//       to: `${email}`,
-//       subject: "test email",
-//       text: "Thank you so much for contacting Living Room Tutors! This is a test. Thanks again, Living Room Tutors.",
-//       html: `<div className="email" >
-//       <h2>Thank you so much for contacting Living Room Tutors!</h2>
-//       <p>This is a test.</p>
-//       <p>Thanks again, Living Room Tutors</p>
-//     </div>`,
-//     })
-//     .catch(console.error);
-//   // console.log("Message sent: %s", info.messageId);
-// });
 
 module.exports = router;
