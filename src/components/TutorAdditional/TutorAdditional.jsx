b1944e545ef2ee85686e5ef59eae064fac38c385
Claire Thallon <clairethallon@gmail.com>
2022-02-17 19:25:09 -0600

    Tutor additional working

--- src/components/TutorAdditional/TutorAdditional.jsx
+++ src/components/TutorAdditional/TutorAdditional.jsx
@@ -11,7 +11,10 @@ import { useHistory } from "react-router-dom";
 
 function StudentAdditional(props) {
   const dispatch = useDispatch();
+
   const store = useSelector((store) => store);
+  const newtutorAdditional = useSelector((store) => store.newtutor.newtutorAdditionalReducer);
+
   const [heading, setHeading] = useState("Tutor Additional Info");
   const history = useHistory();
 
@@ -21,73 +24,87 @@ function StudentAdditional(props) {
 
   useEffect(() => {
     scrollToTop();
+    checkReducer(newtutorAdditional);
+
   }, []);
 
   const scrollToTop = () => {
     window.scrollTo(0, 0);
   };
 
-  const setDefaults = () => {
-    if (presenting) {
-      setNewTutorAdditionalInfo(
-        "I took AP US History last year and scored a 4 on the test, so I would be useful in providing thorough explanations and tips on how the test will go. I am verbally fluent in Arabic and can communicate with multiple dialects. If I do run into some difficulty somehow, my mother is 100% fluent in Arabic (verbal/reading/writing), so there will always be help."
-      );
+
+  const checkReducer = (newtutorAdditional) => {
+    console.log('in checkReducer', newtutorAdditional);
+    if (newtutorAdditional.length == 0) {
+      console.log('EMPTY');
+      return false;
     }
-  };
+    else {
+      setNewTutorAdditionalInfo(newtutorAdditional.tutorAdditionalInfo);
+    }
 
-  const rightArrow = <FontAwesomeIcon icon={faArrowRight} />;
-
-  const changeTutorAdditionalInfo = () => {
-    console.log("in additional info");
-    setNewTutorAdditionalInfo(event.target.value);
-  };
-
-  const AddTutorAdditionalInfo = () => {
-    //package up new info in object
-    const additionalInfo = {
-      tutorAdditionalInfo: newutorAdditionalInfo,
+    const setDefaults = () => {
+      if (presenting) {
+        setNewTutorAdditionalInfo(
+          "I took AP US History last year and scored a 4 on the test, so I would be useful in providing thorough explanations and tips on how the test will go. I am verbally fluent in Arabic and can communicate with multiple dialects. If I do run into some difficulty somehow, my mother is 100% fluent in Arabic (verbal/reading/writing), so there will always be help."
+        );
+      }
     };
-    dispatch({ type: "ADD_TUTOR_ADITIONAL_INFO", payload: additionalInfo });
-    history.push("/TutorTerms");
-  };
 
-  return (
-    <div className="formBackground">
-      <Header />
-      <Container className="formContainer">
-        <div>
-          <TutorProgressBar />
-          <div className="formContent">
-            <div className="formQandA">
-              <p onClick={setDefaults}>
-                Is there any additional information you would like to provide?
+    const rightArrow = <FontAwesomeIcon icon={faArrowRight} />;
+
+    const changeTutorAdditionalInfo = () => {
+      console.log("in additional info");
+      setNewTutorAdditionalInfo(event.target.value);
+    };
+
+    const AddTutorAdditionalInfo = () => {
+      //package up new info in object
+      const additionalInfo = {
+        tutorAdditionalInfo: newutorAdditionalInfo,
+      };
+      dispatch({ type: "ADD_TUTOR_ADITIONAL_INFO", payload: additionalInfo });
+      history.push("/TutorTerms");
+    };
+
+    return (
+      <div className="formBackground">
+        <Header />
+        <Container className="formContainer">
+          <div>
+            <TutorProgressBar />
+            <div className="formContent">
+              <div className="formQandA">
+                <p onClick={setDefaults}>
+                  Is there any additional information you would like to provide?
               </p>
-              <FloatingLabel
-                controlId="AdditionalInfo"
-                label="Additional Info"
-                className="textInput"
-                onChange={(event) => changeTutorAdditionalInfo(event)}
+                <FloatingLabel
+                  controlId="AdditionalInfo"
+                  label="Additional Info"
+                  className="textInput"
+                  onChange={(event) => changeTutorAdditionalInfo(event)}
+                >
+                  <Form.Control
+                    as="textarea"
+                    placeholder="Additional Info"
+                    className="additionalTextArea"
+                    value={newutorAdditionalInfo}
+                    defaultValue={newutorAdditionalInfo}
+                  />
+                </FloatingLabel>
+              </div>
+
+              <Button
+                className="primaryButton saveAndContinueButton"
+                onClick={AddTutorAdditionalInfo}
               >
-                <Form.Control
-                  as="textarea"
-                  placeholder="Additional Info"
-                  className="additionalTextArea"
-                  value={newutorAdditionalInfo}
-                />
-              </FloatingLabel>
+                Save and Continue <span className="rightarrow">{rightArrow}</span>
+              </Button>
             </div>
-
-            <Button
-              className="primaryButton saveAndContinueButton"
-              onClick={AddTutorAdditionalInfo}
-            >
-              Save and Continue <span className="rightarrow">{rightArrow}</span>
-            </Button>
           </div>
-        </div>
-      </Container>
-    </div>
-  );
-}
+        </Container>
+      </div>
+    );
+  }
 
-export default StudentAdditional;
+  export default StudentAdditional;
